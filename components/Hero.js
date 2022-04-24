4aa5bff361eaac75755c72f952303478a426ba60
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-24 19:00:19 +0530

    hero layout

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section>
+            <img src="../images/photo-grid.png" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
