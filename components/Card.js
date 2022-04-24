fe9b581ab854c5a0b1a592a4b72db5837117468a
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-24 19:23:52 +0530

    extra className

--- components/Card.js
+++ components/Card.js
@@ -5,13 +5,13 @@ export default function Card() {
         <div className="card">
             <img src="../images/katie-zaferes.png" className="card--image" />
             <div className="card--stats">
-                <img src="../images/star.png" />
+                <img src="../images/star.png" className="card--star" />
                 <span>5.0</span>
-                <span>(6) • </span>
-                <span>USA</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
             </div>
             <p>Life Lessons with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p><span className="bold">From $136</span> / person</p>
         </div>
     )
 }
