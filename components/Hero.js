4800acc9fe77d7c84057f8c8bc9a8f98d6abc195
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-24 19:06:14 +0530

    add className

--- components/Hero.js
+++ components/Hero.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function Hero() {
     return (
-        <section>
-            <img src="../images/photo-grid.png" />
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by 
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
