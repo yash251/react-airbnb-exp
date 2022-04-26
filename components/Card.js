273cace059f6177fb89bde0047140cf1318e39f8
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-26 12:27:47 +0530

    add item={item}

--- components/Card.js
+++ components/Card.js
@@ -2,25 +2,25 @@ import React from "react";
 
 export default function Card(props) {
     let badgeText;
-    if(props.openSpots === 0) {
+    if(props.item.openSpots === 0) {
         badgeText = "SOLD OUT";
     }
-    else if(props.location === "Online") {
+    else if(props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
         return (
-        <div className="card">
-            {badgeText && <div className="card--badge">{badgeText}</div>} {/* conditional rendering */}
-            <img src={`../images/${props.img}`} className="card--image" />
-            <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+            <div className="card">
+                {badgeText && <div className="card--badge">{badgeText}</div>}
+                <img src={`../images/${props.item.coverImg}`} className="card--image" />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>{props.item.stats.rating}</span>
+                    <span className="gray">({props.item.stats.reviewCount}) • </span>
+                    <span className="gray">{props.item.location}</span>
+                </div>
+                <p className="card--title">{props.item.title}</p>
+                <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
-        </div>
     )
 }
