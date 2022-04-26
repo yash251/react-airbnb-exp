1d2d4c5d3c1548e878022fe038f394941fe5622b
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-26 12:16:46 +0530

    more logic for condtional render

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function Card(props) {
-    return (
+    let badgeText;
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    }
+    else if(props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+        return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} {/* conditional rendering */}
+            {badgeText && <div className="card--badge">{badgeText}</div>} {/* conditional rendering */}
             <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
